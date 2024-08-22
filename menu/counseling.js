8541437110926771cbb9af032c165e9bdefd33da
joincheol <jju161613@naver.com>
2024-08-22 21:52:17 +0900

    Add chat message

--- menu/counseling.js
+++ menu/counseling.js
@@ -1,2 +1,19 @@
-const $input = document.querySelector('#input_text');
-console.log($input.value);
+const $send = document.querySelector('#send');
+const $input_text = document.querySelector('#input_text');
+$input_text.focus();
+const $result_text = document.querySelector('.result_text');
+
+$send.addEventListener('click', function(){
+  let message = `<div class="user">${$input_text.value}</div>`;
+  $result_text.insertAdjacentHTML('beforeend', message);
+  $input_text.value = '';
+  $result_text.scrollTop = $result_text.scrollHeight;
+});
+$input_text.addEventListener('keypress', function(e){
+  if(e.key === 'Enter'){
+    let message = `<div class="user">${$input_text.value}</div>`;
+    $result_text.insertAdjacentHTML('beforeend', message);
+    $input_text.value = '';
+    $result_text.scrollTop = $result_text.scrollHeight;
+  }
+});
